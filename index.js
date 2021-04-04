53113ec886b8322af1ee43d53ddea34f18f9d70f
Carlos <carlos.alfonso.montero@gmail.com>
2021-04-03 23:29:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names,message) {
+    const arrayMessage = [];
+    for (let i = 0; i < names.length; i++) {
+        //arrayMessage[i] = `Thank you, ${names[i]}, for the wonderful ${message} gift!`;
+        arrayMessage.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    }
+    return arrayMessage;
+}
+
+function countDown(number) {
+    let countup = 0;
+    while (countup <= number) {
+        console.log(number);
+        number--;
+    }
+}
